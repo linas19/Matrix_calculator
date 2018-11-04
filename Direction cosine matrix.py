959c1ae46a7d1dac396c37c37d7af6d236cc6de4
Linas <34280196+linas19@users.noreply.github.com>
2018-11-04 14:30:30 +0200

    Create Direction cosine matrix.py

---
+++ Direction cosine matrix.py
@@ -0,0 +1,14 @@
+import  numpy as np
+print("Non linear transport dynamics calculator")
+# krypties kosinusų matrica, arba koordinačių transformacijos matrica:
+# a_11 = int(input())
+# a_12 = int(input())
+# a_13 = int(input())
+# a_21 = int(input())
+# a_22 = int(input())
+# a_23 = int(input())
+# a_31 = int(input())
+# a_32 = int(input())
+# a_33 = int(input())
+i_1 = np.matrix([])
+A = np.matrix([[a_11,a_12,a_13],[a_21,a_22,a_23],[a_31,a_32,a_33]])
