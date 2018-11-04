cf110046cb99b3f721ce34c139171e90a51b16a2
Linas <34280196+linas19@users.noreply.github.com>
2018-11-04 14:35:38 +0200

    Update Momentum-calculator.py

--- Momentum-calculator.py
+++ Momentum-calculator.py
@@ -1,23 +1,27 @@
 import  numpy as np
-print("Non linear transport dynamics calculator. Momentum? (M)")
-whatToDo = input()
-if whatToDo == "M":
-	# A force is acting on a body at point P {F}^T = [Fx,Fy,Fz]. The point is desribed in vectors
-	# {rp}^T=[rpx,rpy,rpz]. Force {F} if momentum {M} is equal: {M}={rp}x{F}=[rp]{F}
-	#Point P vectors:
-	rx = int(input())
-	ry = int(input())
-	rz = int(input())
-	r = np.matrix([[rx],[ry],[rz]])
-	r_asymetric = np.matrix([[0,-rz,ry], [rz,0,-rx],[-ry,rx,0]])
-	#Force F^T:
-	Fx = int(input())
-	Fy = int(input())
-	Fz = int(input())
-	FT = np.matrix([[Fx,Fy,Fz]])
-	F = np.matrix([[Fx], [Fy], [Fz]])
-	#Momentum {M}={rp}x{F}=[rp]{F}:
-	M = r_asymetric * F
+print("Momentum calculator:")
+# A force is acting on a body at point P {F}^T = [Fx,Fy,Fz]. The point is desribed in vectors
+# {rp}^T=[rpx,rpy,rpz]. Force {F} if momentum {M} is equal: {M}={rp}x{F}=[rp]{F}
+#Point P vectors:
+print("rx vector")
+rx = int(input())
+print("ry vector")
+ry = int(input())
+print("rz vector")
+rz = int(input())
+r = np.matrix([[rx],[ry],[rz]])
+r_asymetric = np.matrix([[0,-rz,ry], [rz,0,-rx],[-ry,rx,0]])
+#Force F^T:
+print("Fx vector")
+Fx = int(input())
+print("Fy vector")
+Fy = int(input())
+print("Fz vector")
+Fz = int(input())
+FT = np.matrix([[Fx,Fy,Fz]])
+F = np.matrix([[Fx], [Fy], [Fz]])
+#Momentum {M}={rp}x{F}=[rp]{F}:
+M = r_asymetric * F
 print(M)
 
 # print("Input for x, y, z")
